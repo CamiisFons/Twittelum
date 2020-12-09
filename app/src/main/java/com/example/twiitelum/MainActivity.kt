18651109f13f7c9e80b0ab0af11f3252fcc51d2d
Silva <camila.fonseca.silva@accenture.com>
2020-12-09 18:27:45 -0300

    Adiciona o banco de dados ao projeto

--- app/src/main/java/com/example/twiitelum/MainActivity.kt
+++ app/src/main/java/com/example/twiitelum/MainActivity.kt
@@ -1,28 +1,44 @@
 package com.example.twiitelum
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.Toast
+import caelum.com.twittelumapp.modelo.Tweet
+import com.example.twiitelum.db.TwittelumDatabase
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val botaoPublicar = findViewById<Button>(R.id.criar_tweet)
-
-        botaoPublicar.setOnClickListener {
-            val campoConteudo = findViewById<EditText>(R.id.publicar_tweet)
-            val conteudo = campoConteudo.text.toString()
-
-            Toast.makeText(this, conteudo, Toast.LENGTH_SHORT).show()
-
-            val intecao = Intent(this, ListaActivity::class.java)
-            startActivity(intecao)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.tweet_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.menu_salvar -> {
+            publicaTweet()
+            true
         }
+        android.R.id.home -> {
+            onBackPressed()
+            true
+        }
+        else -> false
+    }
+    fun publicaTweet() {
+        val campoConteudo = findViewById<EditText>(R.id.publicar_tweet)
+        val conteudo = campoConteudo.text.toString()
+        val tweet = Tweet(conteudo)
+        val database = TwittelumDatabase.getDatabase(this)
+        val tweetDao = database.getTweetDao()
+        tweetDao.salva(tweet)
+        Toast.makeText(this, conteudo, Toast.LENGTH_SHORT).show()
+        finish()
+
     }
 }
---
+++ app/src/main/java/com/example/twiitelum/modelo/Tweet.kt
@@ -0,0 +1,10 @@
+package caelum.com.twittelumapp.modelo
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Tweet(val mensagem: String,
+                 @PrimaryKey(autoGenerate = true) val id: Int = 0) {
+
+}
